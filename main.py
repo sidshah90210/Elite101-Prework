8a1109cf4c89e95e70f3ba7bed02da62e60b404c
sidshah90210 <154398111+sidshah90210@users.noreply.github.com>
2024-01-08 02:26:20 +0000

    Refined and completed chatbot

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
+print(
+    "Hey there! I am the Elite101 Prework chatbot. Nice to meet you!\n________________________________________\n\n\n"
+)
 name = input("What is your name? ")
-age = int(input("What is your age? "))
+age = int(input(f"Hi {name}. Could you please enter your age here: "))
+print(
+    f"Oh what I would do to become {age} again. Anyways, {name} how may I assist you today?"
+)
 boolean = True
 while boolean:
   option = int(
       input(
-          "Hi " + name +
-          " how may I assist you today. Please type in the corresponding number value to proceed the options below\n1. ___________ \n2. ___________ \n3. ___________ \n4. ___________ \n5. Exit \n\nEnter number here: "
+          f"\n\nPlease type in the corresponding number value to proceed the options below\n1. ___________ \n2. ___________ \n3. ___________ \n4. ___________ \n5. Exit \n\nEnter number here: "
       ))
 
   if option == 1:
-    print("Thanks for choosing option 1")
+    print("\n\nThanks for choosing option 1\n\n")
   elif option == 2:
-    print("Thanks for choosing option 2")
+    print("\n\nThanks for choosing option 2\n\n")
   elif option == 3:
-    print("Thanks for choosing option 3")
+    print("\n\nThanks for choosing option 3\n\n")
   elif option == 4:
-    print("Thanks for choosing option 4")
+    print("\n\nThanks for choosing option 4\n\n")
   elif option == 5:
-    print("Thank you for using our service! Goodbye and have a great day! ")
+    print(
+        f"Thank you {name} for using our service! Goodbye and have a great day! "
+    )
     boolean = False
   else:
-    print("Please select a valid option.")
+    print("\n\nPlease select a valid option.")
