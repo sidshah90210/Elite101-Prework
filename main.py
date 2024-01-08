371a92a8d7ecadebb6c13911b4d16fa881e7c227
sidshah90210 <154398111+sidshah90210@users.noreply.github.com>
2024-01-08 02:02:52 +0000

    Copied incomplete chatbot from my vscode

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('hello world!!!')
+name = input("What is your name? ")
+boolean = True
+while boolean:
+  option = int(
+      input(
+          "Hi " + name +
+          " how may I assist you today. Please type in the corresponding number value to proceed the options below\n1. ___________ \n2. ___________ \n3. ___________ \n4. ___________ \n5. Exit \n\nEnter number here: "
+      ))
+
+  if option == 1:
+    print("Thanks for choosing option 1")
+  elif option == 2:
+    print("Thanks for choosing option 2")
+  elif option == 3:
+    print("Thanks for choosing option 3")
+  elif option == 4:
+    print("Thanks for choosing option 4")
+  elif option == 5:
+    boolean = False
+  else:
+    print("Please select a valid option.")
